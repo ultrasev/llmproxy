79f89e6e8f5b474582852781276e097c7aaea101
ultrasev <i@ultrasev.com>
2024-05-15 17:46:38 +0800

    feat(usage): add simple usage html

---
+++ vercel/api/usage.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+USAGE = """<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Usage</title>
+    <style>
+        body {
+            font-family: Courier New, monospace;
+            margin: 0;
+            padding: 0;
+            background-color: #000;
+            color: #fff;
+            font-size: 1.5em;
+            line-height: 1.6;
+        }
+
+        .container {
+            max-width: 800px;
+            margin: 50px auto;
+            padding: 20px;
+            background-color: #000;
+            border: 1px solid #999;
+            border-radius: 5px;
+        }
+        h1, p {
+            margin: 0;
+            padding: 0;
+        }
+        a {
+            color: #007bff;
+            text-decoration: none;
+        }
+        a:hover {
+            text-decoration: underline;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <p> success </p>
+        <h1>Usage</h1>
+        <p>Visit <a href="https://dlj.one/b2a7mn" target="_blank">Github doc</a> for more information.</p>
+    </div>
+</body>
+</html>
+
+
+"""
--- vercel/main.py
+++ vercel/main.py
@@ -1,11 +1,18 @@
 #!/usr/bin/env python3
+from api.usage import USAGE as html
 from api.hello import router as hello_router
 from api.v1.openai import router as openai_router
 from api.v1.groq import router as groq_router
 from fastapi import FastAPI
+from fastapi.responses import Response
 app = FastAPI()
 
 
 app.include_router(groq_router, prefix="/v1/groq")
 app.include_router(openai_router, prefix="/v1/openai")
 app.include_router(hello_router, prefix="/hello")
+
+
+@app.get("/")
+def _root():
+    return Response(content=html, media_type="text/html")
